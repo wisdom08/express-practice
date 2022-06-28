c1b1e707fd8c1e08d5899fe804dcf7a9fc88b543
wisdom <wisdom08@kakao.com>
2022-06-28 23:06:10 +0900

    refactor: 싱글톤 패턴 적용

--- express/src/app.ts
+++ express/src/app.ts
@@ -2,23 +2,46 @@ import * as express from "express"
 import catsRouter from "./cats/cats.route";
 
 const port: number = 8000;
-const app: express.Express = express()
 
-app.use((req: express.Request, res: express.Response, next: any) => {
-    console.log(req.rawHeaders[1]);
-    console.log('this is logging middleware');
-    next();
-});
+class Server {
+    public app: express.Application;
 
-app.use(express.json());
-app.use(catsRouter);
+    constructor() {
+        const app: express.Application = express();
+        this.app = app;
+    }
 
+    private setRoute() {
+        this.app.use(catsRouter);
+    }
 
-app.use((req:express.Request, res:express.Response) => {
-    console.log('this is error middleware');
-    res.send({error: '404 not found error'});
-})
+    private setMiddleware() {
+        this.app.use((req: express.Request, res: express.Response, next: any) => {
+            console.log(req.rawHeaders[1]);
+            console.log('this is logging middleware');
+            next();
+        });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+        this.app.use(express.json());
+        this.setRoute();
+
+        this.app.use((req:express.Request, res:express.Response) => {
+            console.log('this is error middleware');
+            res.send({error: '404 not found error'});
+        })
+    }
+
+    public listen() {
+        this.setMiddleware();
+        this.app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+    }
+}
+
+function init() {
+    const server = new Server();
+    server.listen();
+}
+
+init();
